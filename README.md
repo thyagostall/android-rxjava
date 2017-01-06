4b047dce8c8a2224a45a70afe22daf7b6d950333
thyagostall <thstall@gmail.com>
2017-01-06 07:56:29 -0200

    Reset the code

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## RxJava (on Android)
 
-This repository contains all the examples presented in [Dan Lew's series](http://blog.danlew.net/2014/09/15/grokking-rxjava-part-1/).
+This repository contains all the examples presented in [Grokking RxJava series](http://blog.danlew.net/2014/09/15/grokking-rxjava-part-1/) by Dan Lew.
--- app/src/main/java/com/thyago/rxjava/MainActivity.java
+++ app/src/main/java/com/thyago/rxjava/MainActivity.java
@@ -25,84 +25,13 @@ import rx.subjects.Subject;
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-
-    @BindView(R.id.progress_bar)
-    ProgressBar mProgressBar;
-
     private Unbinder mUnbinder;
-
-    private Observable<Integer> mObservable;
-    private Observable<String> mAsyncObservable;
-
-    public void setLoading(final boolean status, View v) {
-        mProgressBar.setVisibility(status ? View.VISIBLE : View.GONE);
-        v.setEnabled(!status);
-    }
-
-    @OnClick(R.id.observe1)
-    @SuppressWarnings("unused")
-    public void onObserve1Click(final View v) {
-        setLoading(true, v);
-        Observer<Integer> myObserver = new Observer<Integer>() {
-            @Override
-            public void onCompleted() {
-                Log.d(LOG_TAG, "Completed");
-                setLoading(false, v);
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.e(LOG_TAG, "Exception");
-            }
-
-            @Override
-            public void onNext(Integer s) {
-                Log.d(LOG_TAG, "-> " + s);
-            }
-        };
-        mObservable
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(myObserver);
-    }
-
-    @OnClick(R.id.observe2)
-    @SuppressWarnings("unused")
-    public void onObservable2Click(final View v) {
-        setLoading(true, v);
-        mAsyncObservable
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(s -> {
-                    Log.d(LOG_TAG, "result=" + s);
-                    setLoading(false, v);
-                });
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mUnbinder = ButterKnife.bind(this);
-
-        mObservable = Observable
-                .from(new Integer[] {1, 2, 3, 4, 5, 6})
-                .map(i -> i * i);
-
-        mAsyncObservable = Observable
-                .create(new Observable.OnSubscribe<String>() {
-                    @Override
-                    public void call(Subscriber<? super String> subscriber) {
-                        Log.d(LOG_TAG, "There request is running.");
-                        try {
-                            Thread.sleep(5000);
-                        } catch (InterruptedException e) {
-                            subscriber.onError(e);
-                        }
-                        subscriber.onNext("Done");
-                        subscriber.onCompleted();
-                    }
-                });
     }
 
     @Override
