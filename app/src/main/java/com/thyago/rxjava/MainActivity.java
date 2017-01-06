27577d61501b2b6c9a6d83bba4f01a76fc3ef027
thyagostall <thstall@gmail.com>
2017-01-06 09:21:59 -0200

    Hello, World!
    - Hello, World!
    - Just
    - Map

--- app/src/main/java/com/thyago/rxjava/MainActivity.java
+++ app/src/main/java/com/thyago/rxjava/MainActivity.java
@@ -17,6 +17,7 @@ import rx.Observer;
 import rx.Scheduler;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
 import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
@@ -40,4 +41,78 @@ public class MainActivity extends AppCompatActivity {
 
         mUnbinder.unbind();
     }
+
+    @OnClick(R.id.button_hello_world)
+    void onHelloWorldClick() {
+        Observable<String> myObservable = Observable.create(
+                new Observable.OnSubscribe<String>() {
+                    @Override
+                    public void call(Subscriber<? super String> subscriber) {
+                        subscriber.onNext("Hello, world!");
+                        subscriber.onCompleted();
+                    }
+                }
+        );
+
+        Subscriber<String> mySubscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() { }
+
+            @Override
+            public void onError(Throwable e) { }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(LOG_TAG, s);
+            }
+        };
+
+        myObservable.subscribe(mySubscriber);
+    }
+
+    @OnClick(R.id.button_hello_world_just)
+    void onHelloWorldJustClick() {
+        final boolean USE_ERROR_AND_COMPLETED = false;
+        final boolean USE_ERROR = true;
+
+        Observable<String> myObservable = Observable.just("Hello, world!");
+
+        Action1<String> onNextAction = s -> Log.d(LOG_TAG, s);
+
+        Action1<Throwable> onErrorAction = t -> Log.d(LOG_TAG, "Erro: " + t);
+
+        Action0 onCompletedAction = () -> Log.d(LOG_TAG, "Completed!");
+
+        if (USE_ERROR_AND_COMPLETED) {
+            myObservable.subscribe(onNextAction, onErrorAction, onCompletedAction);
+        } else if (USE_ERROR) {
+            myObservable.subscribe(onNextAction, onErrorAction);
+        } else {
+            myObservable.subscribe(onNextAction);
+        }
+    }
+
+    @OnClick(R.id.button_transformation_just)
+    void onTransformationJustClick() {
+        Observable.just("Hello, World!")
+                .subscribe(s -> Log.d(LOG_TAG, s + " -Thyago"));
+    }
+
+    @OnClick(R.id.button_transformation_map)
+    void onTransformationMapClick() {
+        Observable.just("Hello, World!")
+                .map(s -> s + " -Thyago")
+                .subscribe(s -> Log.d(LOG_TAG, s));
+
+        Observable.just("Hello, World!")
+                .map(s -> s.length())
+                .map(l -> "The item has length of: " + l)
+                .subscribe(s -> Log.d(LOG_TAG, s));
+
+        Observable.just("Hello, World!")
+                .map(String::length)
+                .map(l -> "The item has length of: " + l)
+                .subscribe(s -> Log.d(LOG_TAG, s));
+    }
+
 }
