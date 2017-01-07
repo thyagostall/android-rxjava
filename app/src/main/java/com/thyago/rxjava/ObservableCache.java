5bf3b38731720e25ae03da7585dc54b86a4566ff
thyagostall <thstall@gmail.com>
2017-01-07 14:57:33 -0200

    Observable cache

---
+++ app/src/main/java/com/thyago/rxjava/ObservableCache.java
@@ -0,0 +1,11 @@
+package com.thyago.rxjava;
+
+import rx.Subscription;
+
+/**
+ * Created by thyago on 07/01/2017.
+ */
+
+public class ObservableCache {
+    public static Subscription subscription;
+}
